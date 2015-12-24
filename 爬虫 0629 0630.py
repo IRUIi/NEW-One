32fa0a4740e274f86c1d91435dae1feb1c419383
IRUIi <825591378@qq.com>
2015-12-24 20:26:05 +0800

    Update 爬虫 0629 0630.py
    对程序进行了了解并加以注释以供使用！

--- 爬虫 0629 0630.py
+++ 爬虫 0629 0630.py
@@ -8,8 +8,8 @@ def getHtml(url):
     return html
 def content(html): 
     str = '<article class="article-content">'# 我们想要得到的内容是在这个目录标签下的
-    content = html.partition(str)[2]# partition() 方法用来根据指定的分隔符将字符串进行分割。这里，区三段中的最后一段
-    str1= '<div class="article-social">'# 功能行为
+    content = html.partition(str)[2]# partition() 方法用来根据指定的分隔符将字符串进行分割。这里，取三段中的最后一段
+    str1= '<div class="article-social">'
     content = content.partition(str1)[0]# 在content目录中取第一段，赋值;就是取得是str后面的加上str1前面的内容
     return content
 def title(content,beg=0):
@@ -56,7 +56,7 @@ def data_out(title, img):
                 img[size] = many_img(img[size])# 多图，调用many_img()方法
             fo.write(title[size]+'$'+img[size]+'\n')# 将标题和图的
         
-def main_content(html):
+def main_content(html):#新的方法用来随意取想看的期数来看
 # 首页内容分割的标签,用来分割首页
     str = '<div class="content">'
     content = html.partition(str)[2]
@@ -64,13 +64,13 @@ def main_content(html):
     content = content.partition(str1)[0]
     return content # 得到网页的内容   
 
-def page_url(content, order = 20, beg = 0):# 新增一个参数order，默认为20
+def page_url(content, order = 20, beg = 0):# 新增一个参数order，默认为20，因为每页20期。分页。
     try:
         url = []
         i = 0
-        while i < order:
+        while i < order:# 一页以内，直接爬取。爬出完整一页
             url1 = content.index('<h2><a href="',beg)+13
-            url2 = content.index('" ',url1)
+            url2 = content.index('" ',url1)#以u11开头的，”结尾的
             url.append(content[url1:url2])
             beg = url2
             i = i + 1
@@ -78,31 +78,30 @@ def page_url(content, order = 20, beg = 0):# 新增一个参数order，默认为
     except ValueError:
         return url   
 def get_order(num):
-# num代表获取的条目数量
+# num代表获取的条目数量，可以改变，就是想看几期，一期就是一条
     url_list = []
     page = num / 20 # 每一页有20条
     order = num % 20 # 超出一整页的条目
     if num < 20:  # 如果获取的条目数量少于20（一页20个），直接爬取第一页的num条
         url = 'http://bohaishibei.com/post/category/main'
         main_html = getHtml(url)
-        clean_content = main_content(main_html)         
-        url_list = url_list + page_url(clean_content, num)  
+        clean_content = main_content(main_html)  # 打开首页，得到网页信息        
+        url_list = url_list + page_url(clean_content, num)  #加入到url_list中，因为不足20期，所以order需要变化，爬出需要的期数。
     for i in range(1, page+1): 
-        url = 'http://bohaishibei.com/post/category/main/page/%d' % i # 爬取整页的条目
+        url = 'http://bohaishibei.com/post/category/main/page/%d' % i # 爬取整页的条目，%i给%d赋值一直爬到最后一页。
         main_html = getHtml(url)
         clean_content = main_content(main_html)
-        url_list = url_list + page_url(clean_content) #获取整夜
+        url_list = url_list + page_url(clean_content) #获取整页，加入到url_list里
       
         if (i == page)&(order > 0):  # 爬到最后一页，如果有超出一页的条目则继续爬order条
-            url = 'http://bohaishibei.com/post/category/main/page/%d' % (i+1) 
+            url = 'http://bohaishibei.com/post/category/main/page/%d' % (i+1) #剩下的下一页里面
             main_html = getHtml(url)
             clean_content = main_content(main_html)         
             url_list = url_list + page_url(clean_content, order)            
-            #print len(page_url(clean_content, order))
     return url_list
-order = get_order(30) # get_order方法接受参数，抓取多少期的数据
+order = get_order(30) # get_order方法接受参数，抓取多少期的数据，return了url_list
 for i in order:  # 遍历列表的方法
-    html = getHtml(i)            
+    html = getHtml(i)   #getHtml(url_list)         
     content_data = content(html)
     title_data = title(content_data)
     img_data = get_img(content_data)
